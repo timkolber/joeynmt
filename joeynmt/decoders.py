517f11c9e43d35c2c737249686be1375a0a0d81b
timkolber <kolber@cl.uni-heidelberg.de>
2022-09-26 22:11:30 +0200

    Fix too many values to unpack

--- joeynmt/decoders.py
+++ joeynmt/decoders.py
@@ -518,7 +518,7 @@ class TransformerTwoPhaseDecoder(Decoder):
         **kwargs,
    
     ):
-        decoder_phase1_output, decoder_phase1_hidden = self.decoder_phase1(
+        decoder_phase1_output, _, _, _ = self.decoder_phase1(
             trg_embed=trg_embed,
             encoder_output=encoder_output,
             encoder_hidden=encoder_hidden,
@@ -528,7 +528,7 @@ class TransformerTwoPhaseDecoder(Decoder):
             trg_mask=trg_mask,
             **kwargs,
         )
-        decoder_phase2_output = self.decoder_phase2(
+        decoder_phase2_output, _, _, _ = self.decoder_phase2(
             trg_embed=trg_embed,
             encoder_output=encoder_output,
             decoder_output=decoder_phase1_output,
